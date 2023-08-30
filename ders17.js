7a4b7efe5aab615350d2848aa9856dd7e051ef17
Musa <Musaxlilov29@gmail.com>
2023-08-30 16:58:08 +0400

    fourth commit

--- ders17.js
+++ ders17.js
@@ -6,10 +6,10 @@ if (note > 100) {
   alert("This note doesn't exist. Minimum note point is: 0. Try again");
 } else {
   if (note >= 80) {
-    alert('Congrulations you passed the exam with a good grade. Your note' + `${note}`);
+    alert('Congrulations you passed the exam with a good grade. Your note: ' + `${note}`);
     console.log('You pass' + `Your note: ${note}`);
   } else if (note >= 50) {
-    alert('You passed the exam but work harder for better result.' + `Your note:${note}`);
+    alert('You passed the exam but work harder for better result.' + `Your note: ${note}`);
     console.log(`You passed. Your note: ${note}`);
   } else if (note <= 50) {
     alert("You couldn't pass the exam. You have to work a lot in this sumer." + `Your note: ${note}`);
