be9c75ded8d63f71143a4af3eca866fe7e81570c
Musa <Musaxlilov29@gmail.com>
2023-08-31 11:11:17 +0400

    new

--- ders19.js
+++ ders19.js
@@ -10,7 +10,7 @@
 let name = prompt('Please enter your name: ');
 let fin = prompt('please enter your fin code:');
 
-
+/*
 check(name , fin);
 
 function check(name , fin) {
@@ -24,4 +24,25 @@ function check(name , fin) {
     alert('insert your name:');
   }
 
+}
+*/
+
+// long story short:
+// if - else : we use this condition for 2 choises
+// if - else if: we use this condition for choises that is more than 2
+// muliple if statement : we use this condition when we want to check each condition seperately 
+
+check2(name , fin);
+
+function check2(name , fin) {
+  if (name === "") {
+    alert('Please enter your name!');
+    return;
+  }
+  if (fin.length!=11) {
+    alert('The fin must be 11 characters.Try again.');
+    return;
+  }
+
+  alert('Your fin and name is inserted correct');
 }
