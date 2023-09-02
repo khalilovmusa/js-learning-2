4f59eada142d213bbc8de1dcd33f0fa89e818123
Musa <Musaxlilov29@gmail.com>
2023-09-02 13:02:52 +0400

    debugger and tooltip use added

---
+++ ders29.js
@@ -0,0 +1,19 @@
+//! ------BREAKPOINT AND TOOLTIP--------
+
+/*
+function findAverage(visa1 , visa2,final) {
+  //codes
+  return 75;
+} 
+*/
+
+let a = 10;
+debugger;
+let b = 15;
+let c = a + b;
+let d = a + b + c;
+let e = a + b + c;
+
+
+let result = a + b + c + d + e;
+console.log("Result" , result);
