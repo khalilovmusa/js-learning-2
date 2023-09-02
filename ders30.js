c27935c9713624cbab3dc703300421693e143eab
Musa <Musaxlilov29@gmail.com>
2023-09-02 15:37:06 +0400

    Loops

---
+++ ders30.js
@@ -0,0 +1,20 @@
+//!---------LOOPS--------
+
+/*
+
+   1-For
+   2-While
+   3-Do-While
+   4-ForEach
+
+*/
+
+ // for (variable; condition ; decrease - increase){
+    //codespace
+ // }
+
+   //EXAMPLE
+
+  for(let i=1 ; i<=10 ; i++) {
+    console.log(i);
+  }
