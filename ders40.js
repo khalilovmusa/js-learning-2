e44063900d06bba9c62f746fb73f8c752ae3f852
Musa <Musaxlilov29@gmail.com>
2023-09-03 20:51:21 +0400

    methods and functions

---
+++ ders40.js
@@ -0,0 +1,28 @@
+//!<---------FUNCTIONS-------->
+
+//parametresiz ve geriye donus yapmayan method tanimlamak
+
+/**
+ * TODO: give your function a logical name so later you look that code you can understood it or someone else can.
+ * function methodname(parameter){
+ *   ?codes
+ * }
+ * 
+ */
+
+
+function print(){
+  console.log("Musa");
+}
+
+//!We always have to call our functions..
+
+print();
+print();
+
+
+function calculation (){
+  console.log(5 + 7);
+}
+
+calculation();
