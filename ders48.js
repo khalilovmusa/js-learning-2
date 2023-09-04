4751682cf39350207d936bddb077010e7610742c
Musa <Musaxlilov29@gmail.com>
2023-09-05 01:39:17 +0400

    decimal to binary converter app2

--- ders48.js
+++ ders48.js
@@ -2,29 +2,32 @@
 
 let number = prompt('Please enter your number :');
 
-function convertToBinary(number){
-  let binary= "";
-
+function convertToBinary (number) {
+  let binary = "";
+  
   while(true){
     binary+= (number%2).toString();
-    number = Math.floor(number / 2);
+    number = Math.floor(number/2);
 
-    if(number === 1){
+    if(number == 1){
       binary += 1;
       break;
     }
-   }
-   let result = reverse(binary);
-   alert(`${result}`);
-   
+
+  }
+
+  let result = reverseBinary(binary);
+  alert(`Your binary code is : ${result}`);
 }
 
-function reverse(binary){
-  let reversedBinary = '';
+
+function reverseBinary(binary){
+  let reversedBinary = "";
   for(let i = binary.length - 1; i >= 0; i--){
     reversedBinary += binary.charAt(i);
 
   }
+
   return reversedBinary;
 }
 
