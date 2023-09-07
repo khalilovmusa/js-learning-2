40a75a050f10836012c153f76f9a9d80c56a7cb9
Musa <Musaxlilov29@gmail.com>
2023-09-07 17:16:42 +0400

    math grade

---
+++ ders59.js
@@ -0,0 +1,74 @@
+//!Math grade methods
+
+/*
+1-floor
+2-ceil
+3-round
+
+4-max
+5-min
+
+6-random
+7-abs
+8-sqrt
+9-pow
+------->PI number
+*/
+
+
+//console.log(window);
+
+//!floor()
+
+let a = 3.25;
+
+console.log(Math.floor(a));
+
+
+//!ceil()
+
+let b = 5.43;
+
+console.log(Math.ceil(b));
+
+
+//!round()
+
+let c = 5.5;
+let d = 5.2;
+
+console.log(Math.round(c));
+console.log(Math.round(d));
+
+//!max====> gives the max number in the array
+
+console.log(Math.max(1,2,3,4,70));
+
+//!min====> gives the min number in the array
+
+console.log(Math.min(-22,-10,-4,2,8));
+
+
+//!abs=====> |-20|= 20
+
+console.log(Math.abs(-100));
+
+
+//!sqrt=====> root of 4 = 2
+
+console.log(Math.sqrt(4));
+
+//!pow
+
+console.log(Math.pow(2,3));
+
+//!pi
+
+console.log(Math.PI);
+
+
+//!random====>gives a random number
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*100))
