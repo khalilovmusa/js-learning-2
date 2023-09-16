b635047f4a3f2956072d1c68e063695ba05fc70d
Musa <Musaxlilov29@gmail.com>
2023-09-16 11:37:02 +0400

    practise with keyboard events

--- ders73.js
+++ ders73.js
@@ -1,7 +1,7 @@
 //!==>Practise with keyboard events
 
 //?==> Lets add a event : If user presses f5 key we'llnot let the user refresh the page
-
+/*
 document.addEventListener("keydown" , run);
 
 function run(e){
@@ -12,4 +12,18 @@ function run(e){
   }
 
   e.preventDefault();
+}
+*/
+
+const header = document.querySelectorAll(".card-title")[0];
+const inputSelect = document.querySelector(".form-control");
+// const addButton = document.querySelectorAll(".btn")[0];
+// console.log(addButton);
+
+inputSelect.addEventListener("keyup" , input);
+
+function input (e) {
+  console.log(e.target.value);
+  header.textContent = e.target.value;
+  
 }
