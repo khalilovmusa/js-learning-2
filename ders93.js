86585f41a64b7d8aee2da93d71414f9869ab2a28
Musa <Musaxlilov29@gmail.com>
2023-09-30 23:15:24 +0400

    Spread operator

---
+++ ders93.js
@@ -0,0 +1,44 @@
+//!==>Spread operator (...)
+
+let numbers =[10,20,30,40];
+function add(a,b,c,d){
+  console.log(a+b+c+d);
+}
+
+
+//?==>Old method
+add(numbers[0],numbers[1],numbers[2],numbers[3]);
+
+
+//?==>New method
+
+add(...numbers);
+
+//TODO==>Explanation of this code
+
+//!===> ...numbers --- numbers[0],numbers[1],numbers[2],numbers[3]
+
+
+
+//TODO==>More examples
+
+const langs1 = ["Java" , "C#"];
+//const langs2 = ["Php" , "Phyton" , langs1[0] , langs1[1]];//!==>The old way
+const langs2 = ["Php" , "Phyton" , ...langs1];//?==>The new way
+
+console.log(langs2);
+
+
+const number = [1,2,3,4,5,6,7,8,9];
+
+let [a,b, ...leftNumbers] = number;
+
+console.log(a,b,leftNumbers);
+
+
+//TODO==> Copying an array
+
+const array1 = ["Musa" , "Mamed" , "Murad" , "Mehman"];
+const array2 = [...array1];
+
+console.log(array2);
